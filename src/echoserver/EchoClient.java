6cd78b9dada940fd458b0a20afaaa9c8c7575762
Cassie Schultz <schu4276@morris.umn.edu>
2020-10-08 11:28:15 -0500

    I have started a skeleton for the client and have gotten it to where it at least compiles. I have created some try catch blcoks and also have attempted to use input stream to get input as bytes

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,40 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
-}
+	public static final int portNumber = 6013;
+
+	public static void main(String[] args) throws IOException{
+	  String server; 
+	  // Use local host if no server specified
+	  if(args.length == 0){
+		server = "127.0.0.1";
+	  } 
+	  else{
+		server = args[0];
+	  }
+
+	  try{
+	  // Connect to server
+	  Socket socket = new Socket(server, portNumber);
+
+	  // read byte form keyboard
+	  InputStream is = null;
+	  int inputbyte = is.read();
+	  while(inputbyte != -1){
+	  	// send byte to server
+		inputbyte = is.read();
+	  }
+	  is.close();
+	  //send single byte to server
+	  //read byte from the server
+	  //print that byte
+	  
+	  }// end try
+	  catch(IOException ioe){
+	    System.out.println("We caught an unexpected exception");
+	  }
+
+	}// end main 
+}// end echoclient
