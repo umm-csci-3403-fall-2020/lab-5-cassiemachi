87955a9ce92feb165607a5a6467f469286ca784e
Cassie Schultz <schu4276@morris.umn.edu>
2020-10-12 22:21:46 -0500

    General skeleton for EchoServer.java I have gotten a start by adding a main method, and created a print writer that will hopefully echo the data it gets from the client

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,34 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+	public static final int portNumber = 6013;
+
+
+	public static void main(String[] args){
+	  try{ 
+	    // Start listening on the specifed port 
+	    ServerSocket sock = new ServerSocket(portNumber);
+
+	    while(true){
+	      Socket client = sock.accept();
+	      System.out.println("Got a request!");
+
+	      // Creates new PrintWriter with autoFlush
+	      InputStream input = socket.getInputStream();
+	      PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+	      
+	      writer.println(input);
+
+	      client.close();
+		}
+	    } catch (IOException ioe){
+		System.out.println("We caught an unexpected error");
+		System.err.println(ioe);
+
+	    }
+
+	}//end main 
+
 }
